46d2c21eba7c5c4f31b91c908a44ee9e67c17c2b
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-10-18 19:18:55 +0200

    add a download icon to the employee dashboard

--- Billed-app-FR-Front/src/views/Actions.js
+++ Billed-app-FR-Front/src/views/Actions.js
@@ -1,12 +1,15 @@
 import eyeBlueIcon from "../assets/svg/eye_blue.js"
 import downloadBlueIcon from "../assets/svg/download_blue.js"
-
+import download from '../assets/svg/download_blue.js';
 export default (billUrl) => {
   return (
     `<div class="icon-actions">
       <div id="eye" data-testid="icon-eye" data-bill-url=${billUrl}>
       ${eyeBlueIcon}
       </div>
+      <a href'#' target="_blank" id="download-link" data-bill-name="${billUrl}">
+      <span id="icon-download"  data-bill-url="${billUrl}"> ${download} </span>
+    </a>
     </div>`
   )
 }
