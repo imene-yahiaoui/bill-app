911ffe3b7ae13a3d87e7ac720998f34082fa047b
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-10-18 19:12:03 +0200

    add  image-to-PDF download functionalit

--- Billed-app-FR-Front/src/app/download.js
+++ Billed-app-FR-Front/src/app/download.js
@@ -1,5 +1,13 @@
-
 export const download = async () => {
-  console.log("Je commence");
- 
-};
+  const { jsPDF } = jspdf;
+  const doc = new jsPDF();
+  const image = new Image();
+  image.src = $("#icon-download").attr("data-bill-url");
+  console.log($("#icon-download").attr("data-bill-url"));
+  doc.addImage(image, "png", 15, 40, 180, 160);
+  const pdfdow = await doc.save(
+    $("#download-link").attr("data-bill-name") + ".pdf"
+  );
+  const downloadLink = $("#download-link");
+ };
+
