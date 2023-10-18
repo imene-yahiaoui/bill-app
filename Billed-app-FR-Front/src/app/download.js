cbb99eaedc1b2d049d54cabaafd61fd87021a67f
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-10-18 19:12:25 +0200

    update image-to-PDF download functionalit

--- Billed-app-FR-Front/src/app/download.js
+++ Billed-app-FR-Front/src/app/download.js
@@ -3,7 +3,6 @@ export const download = async () => {
   const doc = new jsPDF();
   const image = new Image();
   image.src = $("#icon-download").attr("data-bill-url");
-  console.log($("#icon-download").attr("data-bill-url"));
   doc.addImage(image, "png", 15, 40, 180, 160);
   const pdfdow = await doc.save(
     $("#download-link").attr("data-bill-name") + ".pdf"
