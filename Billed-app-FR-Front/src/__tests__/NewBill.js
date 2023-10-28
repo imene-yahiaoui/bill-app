1b3623209762c32a84917c2f4a5d48936c8ac007
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-10-28 21:23:16 +0200

    add a test for the handleChangeFile function

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -1,6 +1,8 @@
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
 import { render, screen, fireEvent } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -10,7 +12,27 @@ describe("Given I am connected as an employee", () => {
       const title = screen.getByTestId("title");
       expect(title.textContent.trim()).toBe("Envoyer une note de frais");
     });
-
+    test("Then it should see input Name of expense", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const inputName = screen.getByTestId("expense-name");
+      expect(inputName).toBeTruthy();
+    });
+    test("Then it should see input datepicker of expense", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const datepicker = screen.getByTestId("datepicker");
+      expect(datepicker).toBeTruthy();
+    });
+    //vat
+    test("Then it should see input vat and pct of expense", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const vat = screen.getByTestId("vat");
+      const pct = screen.getByTestId("pct");
+      expect(vat).toBeTruthy();
+      expect(pct).toBeTruthy();
+    });
     test("Then it should renders NewBill page", () => {
       const html = NewBillUI();
       document.body.innerHTML = html;
@@ -32,6 +54,31 @@ describe("Given I am connected as an employee", () => {
 /**Test de la création d'une instance */
 describe("Given I am connected as an employee", () => {
   describe("When I am submite a NewBill Page,i should  creat NewBill", () => {
+    it("Then handleChangeFile function should be called", () => {
+      const handleChangeFile = jest.fn(screen.handleChangeFile);
+      const files = screen.getAllByTestId("file");
+
+      files.forEach((file) => {
+        file.addEventListener("click", handleChangeFile);
+        userEvent.click(file);
+      });
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(handleChangeFile).toHaveBeenCalledTimes(files.length);
+    });
+    it("Then handleChangeFile function should be called with event", () => {
+      const handleChangeFile = jest.fn(screen.handleChangeFile);
+      const files = screen.getAllByTestId("file");
+      //cree mock pour event
+      files.forEach((file) => {
+        const event = {
+          preventDefault: jest.fn(),
+          target: file,
+        };
+        handleChangeFile(event);
+        expect(handleChangeFile).toHaveBeenCalledWith(event);
+      });
+    });
+
     // Mock des dépendances
     const mockDocument = document;
     const mockOnNavigate = jest.fn();
@@ -47,7 +94,7 @@ describe("Given I am connected as an employee", () => {
         .mockReturnValue(JSON.stringify({ email: "UserEmail" })),
     };
 
-    it("should create an instance of NewBill", () => {
+    it("Then should create an instance of NewBill", () => {
       const newBillInstance = new NewBill({
         document: mockDocument,
         onNavigate: mockOnNavigate,
